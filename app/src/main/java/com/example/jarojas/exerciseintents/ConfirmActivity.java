8f11a2687c1cdc29d8de5b4bb6ef9770913878ce
j-alberto <jalberto.roj@gmail.com>
2016-06-14 00:22:32 -0500

    added intent to confirm data, back(edit) and forth(confirm) navigation

---
+++ app/src/main/java/com/example/jarojas/exerciseintents/ConfirmActivity.java
@@ -0,0 +1,58 @@
+package com.example.jarojas.exerciseintents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Date;
+
+import static com.example.jarojas.exerciseintents.R.id;
+import static com.example.jarojas.exerciseintents.R.string;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    private String name;
+    private int day, month, year;
+    private String phone;
+    private String email;
+    private String description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+
+        name =  getIntent().getStringExtra(getString(R.string.key_name));
+        year =  getIntent().getIntExtra(getString(R.string.key_bdate_year),1900);
+        month =  getIntent().getIntExtra(getString(R.string.key_bdate_month),1);
+        day =  getIntent().getIntExtra(getString(R.string.key_bdate_day),1);
+        phone =  getIntent().getStringExtra(getString(R.string.key_phone));
+        email =  getIntent().getStringExtra(getString(R.string.key_email));
+        description =  getIntent().getStringExtra(getString(R.string.key_descr));
+
+        setTextForTextView(id.detail_tvName, name);
+        setTextForTextView(id.detail_tvBirthdate, String.format("%s/%s/%s", day, month+1, year));
+        setTextForTextView(id.detail_tvPhone, phone);
+        setTextForTextView(id.detail_tvEmail, email);
+        setTextForTextView(id.detail_tvDescription, description);
+
+    }
+
+    public void clickEdit(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(getString(string.key_name), name);
+        intent.putExtra(getString(string.key_phone), phone);
+        intent.putExtra(getString(string.key_email), email);
+        intent.putExtra(getString(string.key_descr), description);
+        intent.putExtra(getString(string.key_bdate_day), day);
+        intent.putExtra(getString(string.key_bdate_month), month);
+        intent.putExtra(getString(string.key_bdate_year), year);
+        startActivity(intent);
+    }
+
+    public void setTextForTextView(int id, String text){
+        ((TextView)findViewById(id)).setText(text);
+    }
+}
--- app/src/main/java/com/example/jarojas/exerciseintents/MainActivity.java
+++ app/src/main/java/com/example/jarojas/exerciseintents/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.jarojas.exerciseintents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import static com.example.jarojas.exerciseintents.R.id;
+import static com.example.jarojas.exerciseintents.R.string;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvName;
+    private TextView tvPhone;
+    private TextView tvEmail;
+    private TextView tvDescription;
+    private DatePicker dpBirthdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvName = (TextView) findViewById(id.tvName);
+        tvPhone = (TextView) findViewById(id.tvPhone);
+        tvEmail = (TextView) findViewById(id.tvEmail);
+        tvDescription = (TextView) findViewById(id.tvDescription);
+        dpBirthdate = (DatePicker) findViewById(id.dpBirthdate);
+
+        String name =  getIntent().getStringExtra(getString(R.string.key_name));
+
+        if (name != null && !name.isEmpty()){
+            setTextFromIntent(tvName, string.key_name);
+            setTextFromIntent(tvPhone, string.key_phone);
+            setTextFromIntent(tvEmail, string.key_email);
+            setTextFromIntent(tvDescription, string.key_descr);
+
+            int day = getIntent().getIntExtra(getString(string.key_bdate_day),1);
+            int month = getIntent().getIntExtra(getString(string.key_bdate_month),1);
+            int year = getIntent().getIntExtra(getString(string.key_bdate_year),1900);
+            dpBirthdate.updateDate(year, month, day);
+        }
+
+
+
+    }
+
+    public void clickNext(View view){
+        Intent intent = new Intent(this, ConfirmActivity.class);
+
+        intent.putExtra(getString(string.key_name), tvName.getText().toString());
+        intent.putExtra(getString(string.key_phone), tvPhone.getText().toString());
+        intent.putExtra(getString(string.key_email), tvEmail.getText().toString());
+        intent.putExtra(getString(string.key_descr), tvDescription.getText().toString());
+
+        intent.putExtra(getString(string.key_bdate_day), dpBirthdate.getDayOfMonth());
+        intent.putExtra(getString(string.key_bdate_month), dpBirthdate.getMonth());
+        intent.putExtra(getString(string.key_bdate_year), dpBirthdate.getYear());
+
+        startActivity(intent);
+    }
+
+    public void setTextFromIntent(TextView textView, int stringId){
+        textView.setText(
+                getIntent().getStringExtra(getString(stringId))
+        );
     }
 }
